a3f0b64fe90e534832f81e9f9d1cfeca4fea18ba
seojw4973 <sgw0816@naver.com>
2024-03-06 20:51:50 +0900

    주석 추가

--- PrimitiveDataType/src/udemy02/MyChar.java
+++ PrimitiveDataType/src/udemy02/MyChar.java
@@ -17,24 +17,24 @@ public class MyChar {
 	}
 
 	public boolean isDigit() {
-		if(c >=48 && c<=57) {		// c가 0~9사이
+		if(c >=48 && c<=57) {		// c가 0~9사이에 속하면 true, 아니면 false
 		return true;
 		}else {
 		return false;
 		}
 	}
 	public boolean isAlphabet() {
-		if(c>=97 && c<=122) {		// c가 a~z 중에 속하는지
+		if(c>=97 && c<=122) {		// c가 a~z 중에 속하면 true, 아니면 false
 		return true;
 		}
-		if(c>=65 && c<=90) {		// c가 A~Z 중에 속하는지
+		if(c>=65 && c<=90) {		// c가 A~Z 중에 속하면 true, 아니면 false
 			return true;
 		}
 		else {
 			return false;
 		}
 	}
-	public boolean isConsonant() {
+	public boolean isConsonant() {		// 알파벳이면서 자음이면 true, 둘 중 하나라도 아니면 false
 		if(isAlphabet() && !isVoewl() ) {
 			return true;
 		}else {
@@ -42,12 +42,12 @@ public class MyChar {
 		}
 	}
 
-	public static void printLowerCaseAlphabets() {
+	public static void printLowerCaseAlphabets() {	// 소문자 a~z 출력
 		 for(char ch ='a';ch<='z';ch++) {
 			 System.out.print(ch);
 		 }
 	}
-	public static void printUpperCaseAlphabets() {
+	public static void printUpperCaseAlphabets() {	// 대문자 A~Z 출력
 		for(char ch ='A';ch<='Z';ch++) {
 			 System.out.print(ch);
 		 }
