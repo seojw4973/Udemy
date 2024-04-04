03d52f3272f09324e66bc8dc4e6438ae85be771a
seojw4973 <sgw0816@naver.com>
2024-04-04 19:30:20 +0900

    상속 초반부

---
+++ _04_Object/src/udemy05/StudentRunner.java
@@ -0,0 +1,9 @@
+package udemy05;
+
+public class StudentRunner {
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.setName("Ranga");
+		student.setEmail("sgw0816@naver.com");
+	}
+}
